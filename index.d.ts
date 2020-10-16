0a0ac2378e08e326a359cd49e82a8893ed1648c1
Angel Angelov <angel.s.angelov@gmail.com>
2020-10-16 22:07:31 +0300

    axios default behavior added

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,7 @@
-import { AxiosRequestConfig } from "axios";
+import { AxiosRequestConfig, AxiosStatic } from "axios";
 
 export class Http {
+    public axiosInstance: AxiosStatic
     public get<T>(url: string, type: (new (arg: any) => T), useConstructor: Boolean, config?: AxiosRequestConfig | undefined): Promise<any> 
     public delete<T>(url: string, type: (new (arg: any) => T), useConstructor: Boolean, config?: AxiosRequestConfig | undefined): Promise<any> 
     public post<T>(url: string, payload: any, type: (new (arg: any) => T), useConstructor: Boolean, config?: AxiosRequestConfig | undefined): Promise<any> 
